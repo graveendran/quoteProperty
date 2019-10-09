c3091995b709d67cfb0c3cc24aa545c10de2e899
Giridhar Raveendran <graveendran@outlook.com>
2019-10-09 17:07:09 -0400

    Added logging and fixing effectiveDate to be today

--- chrome.py
+++ chrome.py
@@ -5,6 +5,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from datetime import date
+from datetime import datetime
 
 from shutil import copyfile
 
@@ -14,7 +15,8 @@ import os
 import time
 import csv
 import config
-
+import logging
+import logging.handlers
 
 # https://www.python.org/downloads/
 # pip install selenium
@@ -37,7 +39,7 @@ class harmony():
 		
 	def login(self, url, username, password):
 		self.driver.get(url)
-		print('################Login into System#########################')
+		logging.info('################Login into System#########################')
 		element = ui.WebDriverWait(self.driver, 15).until(EC.visibility_of_element_located((By.NAME, "username")))
 		element.send_keys(username)
 		element = ui.WebDriverWait(self.driver, 15).until(EC.visibility_of_element_located((By.NAME, "password")))
@@ -50,7 +52,7 @@ class harmony():
 		return element
 	
 	def gotoHarmony(self, url, addressToQuote, firstName, lastName, agencyEmail, phoneNumber, effectiveDate, shareName, shareEmail):
-		print('################Search Address#########################')
+		logging.info('################Search Address#########################')
 		self.driver.get(url + "search/address")
 		product = Select(ui.WebDriverWait(self.driver, 15).until(EC.visibility_of_element_located((By.ID, "product"))))
 		product.select_by_index(1)
@@ -59,11 +61,11 @@ class harmony():
 		element.send_keys(Keys.RETURN)
 		time.sleep(2)
 		
-		print('################Click First Address#########################')
+		logging.info('################Click First Address#########################')
 		element = ui.WebDriverWait(self.driver, 15).until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".survey-wrapper ul li")))
 		element.click()
 		
-		print('################Fill Contact Info#########################')
+		logging.info('################Fill Contact Info#########################')
 		element = ui.WebDriverWait(self.driver, 15).until(EC.visibility_of_element_located((By.ID, "policyHolders[0].firstName")))
 		element.send_keys(firstName)
 		
@@ -89,11 +91,11 @@ class harmony():
 				element = ui.WebDriverWait(self.driver, 15).until(EC.visibility_of_element_located((By.ID, "agentCode")))
 				element.send_keys(Keys.DOWN)
 
-		print('################Submit Contact Info#########################')
+		logging.info('################Submit Contact Info#########################')
 		self.driver.find_element_by_css_selector('.btn.btn-primary').click()
 		time.sleep(5)
 
-		print('################Fill Underwriting Questions#########################')
+		logging.info('################Fill Underwriting Questions#########################')
 		element = ui.WebDriverWait(self.driver, 15).until(EC.visibility_of_element_located((By.ID, "underwritingAnswers.rented.answer")))
 		
 		element = self.driver.find_element_by_xpath('//span[@data-test="underwritingAnswers.rented.answer_Never"]')
@@ -109,52 +111,52 @@ class harmony():
 			element = self.driver.find_element_by_xpath('//span[@data-test="underwritingAnswers.fourPointUpdates.answer_Yes"]')
 			self.driver.execute_script("arguments[0].click();", element)
 		except NoSuchElementException:
-			print('################ Not Found: UW Four Point Updates #########################')
+			logging.error('################ Not Found: UW Four Point Updates #########################')
 
 		try:
 			element = self.driver.find_element_by_xpath('//span[@data-test="underwritingAnswers.business.answer_No"]')
 			self.driver.execute_script("arguments[0].click();", element)
-			print('################ UW No Business #########################')
+			logging.info('################ UW No Business #########################')
 		except NoSuchElementException:
-			print('Not Found: UW No Business')
+			logging.error('Not Found: UW No Business')
 
-		print('################Submit Underwriting Answers#########################')
+		logging.info('################Submit Underwriting Answers#########################')
 		self.driver.find_element_by_css_selector('.btn.btn-primary').click()
 		time.sleep(5)
 
-		print('################Submit Coverage Details #########################')
+		logging.info('################Submit Coverage Details #########################')
 		element = ui.WebDriverWait(self.driver, 15).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="QuoteWorkflow"]/div[13]/div')))
 		element.click()
 		time.sleep(5)
 		self.driver.find_element_by_css_selector('.btn.btn-primary').click()
 		time.sleep(5)
-		print('################ Click Submit Button Coverage #########################')
+		logging.info('################ Click Submit Button Coverage #########################')
 		element = ui.WebDriverWait(self.driver, 15).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="QuoteWorkflow"]/div[28]/div/button')))
 		element.click()
 		# time.sleep(5)
 		# self.driver.find_element_by_css_selector('.btn.btn-primary').click()
 		try:
-			print('################ Submit Share Quote Summary #########################')
+			logging.info('################ Submit Share Quote Summary #########################')
 			element = ui.WebDriverWait(self.driver, 15).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="QuoteWorkflow"]/div[1]/button[2]')))			
 			element = self.driver.find_element_by_xpath("//form[@id='QuoteWorkflow']/div[1]/button[1]")
 			element.click()
-			print('################ Click Share Quote Button #########################')
+			logging.info('################ Click Share Quote Button #########################')
 			# time.sleep(5)
-			print('################ Fill Share Infor #########################')
+			logging.info('################ Fill Share Infor #########################')
 			element = ui.WebDriverWait(self.driver, 15).until(EC.visibility_of_element_located((By.ID, "toName")))
 			element.send_keys(shareName)
 			
 			element = ui.WebDriverWait(self.driver, 15).until(EC.visibility_of_element_located((By.ID, "toEmail")))
 			element.send_keys(shareEmail)
 			
-			print('################ Press Enter to Submit #########################')
+			logging.info('################ Press Enter to Submit #########################')
 			element = ui.WebDriverWait(self.driver, 15).until(EC.visibility_of_element_located((By.XPATH, '//form[@id="sendQuoteSummary"]/div[2]/button[2]')))
-			print('################ Click Send Email Button ########################')
+			logging.info('################ Click Send Email Button ########################')
 			element.click()
 		except TimeoutException:
-			print('Not Found: Share Quote Button', element, addressToQuote)
+			logging.error('Not Found: Share Quote Button', element, addressToQuote)
 		except NoSuchElementException:
-			print('Not Found: Share Quote Button', element, addressToQuote)
+			logging.error('Not Found: Share Quote Button', element, addressToQuote)
 		
 	def shutdown(self):
 		self.driver.close()
@@ -193,11 +195,13 @@ class harmony():
 		effDate = effDate[0] + "/" + day + "/" + "2019"
 
 		todayDate = date.today().strftime('%m/%d/%Y')
-		effDate = time.strftime('%m/%d/%Y')
-		if (effDate >= todayDate):
-			effDate = todayDate
-
-		return effDate
+		compareTodayDate = datetime.strptime(todayDate, '%m/%d/%Y')
+		compareEffDate = datetime.strptime(effDate, '%m/%d/%Y')
+		# effDate = effDate.strptime('%m/%d/%Y')
+		if (compareEffDate >= compareTodayDate):
+			return effDate
+		else:
+			return todayDate
 	
 	def buildPropertyAddress(self, streetNumber, streetPrefix, streetName, streetUnit, city, zip):
 		address = ' '.join([
@@ -211,7 +215,17 @@ class harmony():
 		return address
 		
 if __name__ == "__main__":
-	print("****************STARTING QUOTING********************")
+	folderPath = os.path.dirname(__file__)
+	logFolder = folderPath + "\\log\\"
+	if not os.path.exists(logFolder):
+		os.makedirs(logFolder)
+
+	logFileName = 'quoteLogs-' + date.today().strftime("%Y%m%d-%H%M%S") + '.log'
+	
+	logging.basicConfig(filename = logFolder + logFileName, 
+											level = logging.INFO,
+											format = '%(asctime)s:%(levelname)s:%(message)s')
+	logging.info("****************STARTING QUOTING********************")
 	obj = harmony()
 
 	url = config.webdetails['url']
@@ -227,10 +241,10 @@ if __name__ == "__main__":
 	
 	listOfAddr = obj.readAddress(filePath)
 	obj.login(url, username, password)
-	print("****************WAITING TO SIGN-IN********************")
+	logging.info("****************WAITING TO SIGN-IN********************")
 	obj.waitForLoading()
-	print("****************SIGNED IN********************")
-	print("****************READING THE LIST********************")
+	logging.info("****************SIGNED IN********************")
+	logging.info("****************READING THE LIST********************")
 	for row in listOfAddr:
 		address = obj.buildPropertyAddress(row['SiteStreetNumber'], row['SiteStreetPrefix'], 
 								row['SiteStreetName'], row['SiteStreetUnit'], row['SiteCity'], row['SiteZip'])
@@ -240,11 +254,11 @@ if __name__ == "__main__":
 
 		effectiveDate = obj.getEffectiveDate(saleDate)
 		
-		print("****************QUOTING********************")
-		print(address, firstName, lastName, effectiveDate)
+		logging.info("****************QUOTING********************")
+		logging.info(address, firstName, lastName, effectiveDate)
 
 		obj.gotoHarmony(url, address, firstName, lastName, agencyEmail, phoneNumber, effectiveDate, shareName, shareEmail)
 	 	obj.waitForLoading()
 	
 	obj.shutdown()
-	print('#######################End########################')
+	logging.info('#######################End########################')
